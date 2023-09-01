efbff1851fcf57b996f28c696c4df6dafe0f4527
Chase Miller <cpmills26@gmail.com>
2023-09-01 12:03:38 -0400

    Renamed 'App.rs' to "main.rs'

---
+++ target/debug/deps/playground-d281d22338aeb716.d
@@ -0,0 +1,5 @@
+C:\Users\cpmil\OneDrive\Documents\GitHub\Playground\target\debug\deps\playground-d281d22338aeb716.rmeta: src\main.rs
+
+C:\Users\cpmil\OneDrive\Documents\GitHub\Playground\target\debug\deps\playground-d281d22338aeb716.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/playground-f1c69ba9f8d80180.d
@@ -0,0 +1,5 @@
+C:\Users\cpmil\OneDrive\Documents\GitHub\Playground\target\debug\deps\playground-f1c69ba9f8d80180.rmeta: src\main.rs
+
+C:\Users\cpmil\OneDrive\Documents\GitHub\Playground\target\debug\deps\playground-f1c69ba9f8d80180.d: src\main.rs
+
+src\main.rs:
